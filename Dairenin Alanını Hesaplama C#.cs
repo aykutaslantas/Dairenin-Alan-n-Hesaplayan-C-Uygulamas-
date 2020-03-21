14e8925734b76c13e123c7dd2fe75b5b4f765063
Aykut İsmet Aslantaş <aykur111aykur@gmail.com>
2020-03-21 17:42:09 +0300

    Add files via upload

---
+++ Dairenin Alanını Hesaplama C#.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dairenin_Alanını_Hesaplamaaa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double pi = 3.14;
+            double yaricap, alan;
+            Console.Write("Dairenin Yarıçapını Girin :");
+            yaricap = Convert.ToDouble(Console.ReadLine());
+            alan = pi * yaricap * yaricap;
+            Console.Write("Yarıçapını {0} Girdiğiniz Dairenin Alanı :{1}", yaricap, alan);
+            Console.ReadKey();
+                
+        }
+    }
+}
